c9288bc663baaa31f123c53a01f3d05926c7f2e5
RKN <rumia5930@gmail.com>
2023-05-10 19:23:21 +0300

    created quiz_questions_and_answers

---
+++ lib_questions_and_answers.py
@@ -0,0 +1,14 @@
+quiz_questions_and_answers = {
+    '1.What is the name of the current capital of Japan?': 'tokyo',
+    '2.What is the historical period between 300 to 538 AD called?': 'kofun',
+    '3.During which period was "Genji Monogatari" written in?': 'Heian',
+    '4.What is the translation of this kanji in English: 中国': 'china',
+    '5.What is the translation of this word in English: ブルガリア': 'bulgaria',
+    '6.Which clan ruled Japan during the Edo period': 'Tokugawa',
+    '7.What are the indigenous people living in Hokkaido called?': 'ainu',
+    '8.In which city is Fushimi Inari Shrine?': 'kyoto',
+    '9.Who is the goddess of the sun in the Japanese mythology?': 'amaterasu',
+    '10.What era started on May 1st 2019?': 'reiwa',
+    '11.In which prefecture was Osamu Dazai born in?': 'aomori',
+    '12.What era began in 1868?': 'meiji'
+}
