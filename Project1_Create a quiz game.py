634b0d11f967508be5b53008b2cf871923007eee
RKN <rumia5930@gmail.com>
2023-05-10 19:25:50 +0300

    mess

---
+++ Project1_Create a quiz game.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+from lib_questions_and_answers import quiz_questions_and_answers as QandA
+
+def get_user_name():
+    name = input('Please, enter your name: ')
+    print(f'''Hello, {name} and welcome to the quiz!
+The goal here is to answer 12 questions.
+You have 3 lives''')
+    return name
+
+def start_game(name):
+    score = 0
+    lives = 3
+    for question, answer in QandA.items():
+        user_input = input(question + ': ').lower()
+        if user_input == answer.lower():
+            print('Correct! Keep going!')
+            score += 1
+        else:
+            print('Wrong answer!')
+            lives -= 1
+            if lives == 0:
+                print('Quiz is over')
+                break
+    print(f'{name}, here is your final score:\n'+'Score =', score, 'Lives =', lives)
+
+def main():
+    name = get_user_name()
+    start_game(name)
+
+main()
